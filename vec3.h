ab69554a999c3f2e08b1ac7feac7485bce1f89af
aashiq <ashaik11@asu.edu>
2023-06-28 21:38:35 -0700

    added lambertian diffuse method

--- vec3.h
+++ vec3.h
@@ -146,4 +146,22 @@ vec3 random_in_unit_sphere()
             }
         }
 
+vec3 random_unit_vector()
+{
+    return unit_vector(random_in_unit_sphere());
+}
+
+vec3 random_in_hemisphere(const vec3& normal)
+{
+    vec3 in_unit_sphere =  random_in_unit_sphere();
+    if(dot(in_unit_sphere, normal) > 0.0) // in the same hemisphere as the normal
+    {
+        return in_unit_sphere;
+    }
+    else
+    {
+        return -in_unit_sphere;
+    }
+}
+
 #endif

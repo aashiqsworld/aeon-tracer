94bf1a8343b753920a646f4373321b5e93a76550
aashiq <ashaik11@asu.edu>
2023-06-29 19:48:06 -0700

    added dielectric materials

--- material.h
+++ material.h
@@ -39,19 +39,40 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
 
     public:
         color albedo;
+		double fuzz;
+};
+
+class dielectric : public material {
+	public: dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+	virtual bool scatter(
+		const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+	) const override {
+		attenuation = color(1.0, 1.0, 1.0);
+		double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+		vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+		scattered = ray(rec.p, refracted);
+		return true;
+	}
+
+	public:
+		double ir; // index of refraction
 };
 
 
